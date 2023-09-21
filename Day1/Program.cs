41c75d9d09b8bd85778c3ec117294c977a07e4de
= <=>
2023-09-21 11:50:57 +0100

    Variables in c#

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -7,7 +7,31 @@ namespace TestProject
     {
     public  static void Main(string[] args) {
                 
+        //VARIABLES
 
+        //  Integers      
+        // int  x = 5;                 
+        // int  y = 3;  
+        // int  z = -1;
+
+         //Float
+         // float y = -1.0f;
+        // float x = 3.0f;        
+        // float z = 0.8f;
+
+        // String
+        // string string1 ="a";
+        // string string2 ="bird";
+        // string string3 ="I eat pizza";
+
+        // Char
+            // char chart1 = '@';
+            // char chart2 = '2';
+            // char chart3 = '3';
+
+        //Boolean
+        // bool complete = false;
+        // bool incomplete = true;
         
 
 
