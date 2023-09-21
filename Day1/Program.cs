649f597b7de3c71206cb2296a30349c4f754e9e5
= <=>
2023-09-21 12:02:47 +0100

    CONDITIONAL STATEMENTS in C#

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -32,6 +32,34 @@ namespace TestProject
         //Boolean
         // bool complete = false;
         // bool incomplete = true;
+
+        //**********************************************************************************//
+
+                        // CONDITIONAL STATEMENTS
+
+      /*
+       Console.Write("Please enter your age: ");
+       int age = Convert.ToInt32(Console.ReadLine());
+
+       if(age > 38)
+       {
+           Console.WriteLine("You are too old to sign up ");
+       }
+       else if (age < 0)
+       {
+           Console.WriteLine("You haven't been born yet");
+       }
+       else if
+               (age >= 17)
+               {
+                   Console.WriteLine("You have now signed up! ");
+               }                               
+       else
+       {
+           Console.WriteLine("You must be 17+ to sign up");    
+        }
+
+        */
         
 
 
