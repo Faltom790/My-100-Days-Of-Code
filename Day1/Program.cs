15fbed68b751af6bb606b672aad694c94040d406
= <=>
2023-09-21 02:36:32 +0100

    Making Changes

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args) {
+                
+                 //VARIABLES
+            //  INTEGERS       //FLOAT 
+
+        // int  x = 5;         float x = 3.0f;
+        // int  y = 3;         float y = -1.0f;
+        // int  z = -1;        float z = 0.8f;
+            
+
+
+
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # My-100-Days-Of-Code
 My journal for the #100DaysOfCode challenge coding in C#
+
+
+## Day 1
+I learnt about the C# main method, Variables, Conditional Statements..........[more](Day1/Day1.md)
+
+## Day 2
+
