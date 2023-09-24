b45e541a36d085a8c84d2294c208dfbeb43c9df2
= <=>
2023-09-24 23:51:20 +0100

    modified:   Day4/Day4.md 	modified:   Day4/Program.cs

--- Day4/Day4.md
+++ Day4/Day4.md
@@ -5,7 +5,8 @@
 
 Day [4] - [24th September, 2023]
 Today's Progress:
-I learnt about the C# data types, Numbers, Math Class, Strings, Checking String equality, @ and $ with strings.
+I learnt about the C# data types, Numbers, Math Class, Strings, Checking String equality, @ and $ with strings,Formatting strings,Stringbuilder class and Validating strings using string.IsNullOrEmpty
+
 
 Goals for Tomorrow:
-I plan on learning Slicing strings into substrings, String escape sequence Formatting strings,Stringbuilder class.
+I plan on learning Booleans, Logical Operators, Utilizing the boolean field, Null, Converting Variables Boxing and Unboxing,Value and Reference and Anonymous and Dynamic Types.
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,16 +1,52 @@
 ﻿using System;
-using System.Diagnostics;
+using System.Text;
 
 namespace TestProject
 {
     class Program
     {
-        public  static void Main(string[] args) {
-        
-        string example = "abc";
-        Console.WriteLine(example.Contains("bc"));
+        public static void Main(string[] args)
+        {
 
 
+            //Slicing strings into substrings
+            // string example = "abc";
+            // Console.WriteLine(example.Substring(0,1));
+
+            //String Escape Sequences
+
+            // " \ " symbol
+            //   Console.WriteLine("Bobby said \"Go write some code\"");
+
+            // "\\"
+            //Console.WriteLine("a\\b");
+
+            // "\t" (Tab Command)
+            //   Console.WriteLine("Hllo \t World");
+
+
+            //FORMATTING STRINGS
+
+            // "{0:c}" (displays currency)
+            // int price1 = 50;
+            // int price2 = 150;
+            // int price3 = 200;
+            // Console.WriteLine(string.Format("{0:c}",price1));
+
+            // "{0:p}" (displays percentage %)
+            // float percent = 0.5f;
+            // Console.WriteLine(string.Format("The task is {0:P}",percent));
+
+
+            //STRING BUILDER 
+            //  StringBuilder stringBuilder = new StringBuilder();
+            //  stringBuilder.Append("nice to see you");
+
+            //  string call = stringBuilder.ToString();
+            //  Console.WriteLine(call);
+
+             StringBuilder sb = new StringBuilder("I am a boy");   
+             
         }
     }
 }
