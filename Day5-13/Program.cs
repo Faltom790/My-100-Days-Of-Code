6b09b223f157a147c1c0d91f2dd2df9042a7ce15
= <=>
2023-10-04 11:38:05 +0100

    renamed:    Day5and6/Day5and6.csproj -> Day5-13/Day5-13.csproj 	new file:   Day5-13/Day5-13.md 	new file:   Day5-13/Program.cs 	deleted:    Day5and6/Program.cs

---
+++ Day5-13/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace TestProject
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine(DateTime.Now);
+
+            //DateTime datetime = new DateTime(2020, 1, 19);
+            //Console.WriteLine(datetime);
+
+            // CONTROL Flow Statements and Loops
+
+            // if(5 < 3){
+            //     Console.WriteLine("5 is less than 3");
+            // }
+            // else if(4 < 2){
+            //     Console.WriteLine("2 is less than 4");
+            // }else
+            // {
+            //     Console.WriteLine("Else Statement was hit ");
+            // }
+
+            //SWITCH CASE
+
+            // int value = 11;
+            // switch (value)
+            // {
+            //     case 1:
+            //     Console.WriteLine("Num One");
+            //     break;
+            //     case 2:
+            //     Console.WriteLine("Num Two");
+            //     break;
+                
+            //     default: 
+            //     Console.WriteLine("other");
+            //     break;        
+            // }
+              
+                //LOOPS (FOR LOOP)
+            // for (int i = 1; i < 6; i++)
+            // {
+            //     Console.WriteLine("Hello World") ;
+            // }
+
+            // string example = "abc";  
+            // for (int i = example.Length - 1; i >= 0; i--)
+            // {
+            //     Console.WriteLine(example[i]);
+            // }
+
+            //FOREACH LOOPS
+            // foreach (var item in "abc")
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            //WHILE LOOPS
+            while ()
+            {
+                
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
